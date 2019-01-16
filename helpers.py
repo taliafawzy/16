beb25f62bd377dad73ea9c8958147934b5d5e587
Talia Fawzy <talia_fawzy@hotmail.com>
2019-01-16 12:23:06 +0000

    helpers, twee errors worden later gefixt

--- helpers.py
+++ helpers.py
@@ -1,8 +1,13 @@
 import csv
 import urllib.request
+from cs50 import SQL
+
 
 from flask import redirect, render_template, request, session
 from functools import wraps
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///16.db")
 
 
 def apology(message, code=400):
@@ -38,56 +43,36 @@ def getResults(keyword):
 
 
 
+def tried_recipe(recipe):
+
+    # update amount of recipes tried in portfolio
+    db.execute("UPDATE portfolio SET tried = :tried WHERE userid = :userid", tried=+1, userid = session["userid"])
+
+    # update boolean 'tried' in cookbook
+    db.execute("UPDATE cookbook SET tried = :tried WHERE userid = :userid", tried = True, userid = session["userid"])
+
+
+def save_recipe(recipe):
+    # update amount of recipes saved in portfolio
+    db.execute("UPDATE portfolio SET saved = :saved WHERE userid = :userid", saved=+1, userid = session["userid"])
+
+    # register recipe into cookbook
+    db.execute("INSERT INTO cookbook (recipe, link, tried, rated) VALUES(:recipe, :link, :tried, :rated) WHERE userid = :userid", recipe = recipe, link = link, tried = False, rated = 0, userid = session["userid"])
+
+
+def personal_rating(rating):
+    # update rating of recipe in cookbook
+    db.execute("UPDATE cookbook SET rated =:rated WHERE userid = :userid", rated = rating, userid = session["userid"])
+
+
+def common_rating(single_rating):
+    # update rating of recipe in recipe
+    db.execute("UPDATE recipe SET rating = :rating / :people AND people = :people WHERE userid = :userid", rating = rating + single_rating, people=+1, userid = session["userid"])
 
 
 
 
 
-@app.route("/tried", methods=["GET", "POST"])
-def personaltried():
 
-    # insert into database if recipe clicked "tried"
-    db.execute("INSERT INTO portfolio (tried) VALUES (:tried)")
-
-    #sum all tried recipes
-    db.execute("SELECT tried, SUM(tried) AS tried, tried FROM portfolio WHERE UserID = :id",  id=session["user_id"])
-
-@app.route("/rated", methods=["GET", "POST"])
-def allrated():
-
-    # insert into database if recipe clicked "rated"
-    db.execute("INSERT INTO cookbook (rated) VALUES (:rated)")
-
-    #average all rated recipes
-	averageRated = db.execute("SELECT * FROM rated, SELECT AVG(rated))
-
-@app.route("/tried", methods=["GET", "POST"])
-	def alltried():
-
-	    # insert into database if recipe clicked "rated"
-	    db.execute("INSERT INTO cookbook (tried) VALUES (:tried)")
-
-	    #select top 2 tried recipes
-	    bestTwo = db.execute("SELECT TOP 2 * FROM tried, tried FROM cookbook WHERE ingredient = request.form.get("ingredient"))
-
-@app.route("/rated", methods=["GET", "POST"])
-def personalrated():
-
-    # insert into database if recipe clicked "rated"
-    db.execute("INSERT INTO portfolio (rated) VALUES (:rated)")
-
-    #sum all rated recipes
-    db.execute("SELECT rated, SUM(rated) AS rated, rated FROM portfolio WHERE UserID = :id",  id=session["user_id"])
-
-
-
-@app.route("/saved", methods=["GET", "POST"])
-def personalsaved():
-
-    # insert into database if recipe clicked "saved"
-    db.execute("INSERT INTO portfolio (saved) VALUES (:saved)")
-
-    #sum all saved recipes
-    db.execute("SELECT saved, SUM(saved) AS saved, saved FROM portfolio WHERE UserID = :id",  id=session["user_id"])
 
 
