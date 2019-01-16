ca9204ce832ebb94181f5adcd3d4c6c9de64266b
Talia Fawzy <talia_fawzy@hotmail.com>
2019-01-16 12:23:44 +0000

    database in application aangepast

--- application.py
+++ application.py
@@ -25,7 +25,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///16.db")
 
 
 @app.route("/login", methods=["GET", "POST"])
